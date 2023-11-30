e6017757f45aba9134ce3d1d4efb13acccf828a9
tugruldonmez <cavslebron@hotmail.com>
2023-11-30 15:01:22 +0300

    signup modal

--- reactsourize/src/components/user/SignUp.js
+++ reactsourize/src/components/user/SignUp.js
@@ -4,10 +4,10 @@ import'../base/base.css';
 import axios from 'axios';
 
 const SignUp = ({ onClose }) => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [selectedItems, setSelectedItems] = useState([]);
 
 
   const navigate = useNavigate();
@@ -15,33 +15,30 @@ const SignUp = ({ onClose }) => {
   const handleSignUp = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://127.0.0.1:3001/users/signup', {
+      const response = await fetch('http://localhost:8080/public/register', {
         method: 'POST',
         headers: {
           accept: 'application/json',
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
+          firstName,
+          lastName,
           username,
-          email,
           password,
-          selectedItems
+          authorities: ['ROLE_USER'], // ROLE_USER'ı ekleyin
         }),
       });
 
       if (!response.ok) {
         throw new Error('Failed to sign up. Please try again.');
       }
-      const dataToSendDjango = {
-        "old_pref":[],
-        "curr_pref": selectedItems,
-      };
-      try {
-        await axios.post('http://localhost:8000/ScrapeReddit/add_subreddits/', dataToSendDjango);
-        console.log('Data sent successfully!');
-      } catch (error) {
-        console.error('Error sending data:', error);
-      }
+      // try {
+      //   await axios.post('http://localhost:8000/ScrapeReddit/add_subreddits/', dataToSendDjango);
+      //   console.log('Data sent successfully!');
+      // } catch (error) {
+      //   console.error('Error sending data:', error);
+      // }
 
       if (!response.ok) {
         throw new Error('Failed to sign up. Please try again.');
@@ -62,29 +59,33 @@ const SignUp = ({ onClose }) => {
     setUsername(e.target.value);
   };
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
+  const handleFirstName = (e) => {
+    setFirstName(e.target.value);
+  };
+  const handleLastName = (e) => {
+    setFirstName(e.target.value);
   };
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
-  };
-
-  const handleSelectedItems = (selectedItems) => {
-    setSelectedItems(selectedItems); // Update the selectedItems state
   };
   return (
     <div className="signup-modal">
       <h2 className='better-htags'>Kayıt Ol</h2>
       <form onSubmit={handleSignUp}>
         <div className="form-group">
+          <label className='better-labels' htmlFor="email">İsim: </label>
+          <input className='' type="text" id="firstName" value={firstName} onChange={handleFirstName} />
+        </div>
+        <div className="form-group">
+          <label className='better-labels' htmlFor="email">Soyisim: </label>
+          <input className='' type="text" id="lastName" value={lastName} onChange={handleLastName} />
+        </div>
+        <div className="form-group">
           <label className='better-labels' htmlFor="username">Kullanıcı Adı: </label>
           <input className='' type="text" id="username" value={username} onChange={handleUsernameChange} />
         </div>
-        <div className="form-group">
-          <label className='better-labels' htmlFor="email">E-posta: </label>
-          <input className='' type="email" id="email" value={email} onChange={handleEmailChange} />
-        </div>
+
         <div className="form-group">
           <label className='better-labels' htmlFor="password">Şifre: </label>
           <input className='' type="password" id="password" value={password} onChange={handlePasswordChange} />
