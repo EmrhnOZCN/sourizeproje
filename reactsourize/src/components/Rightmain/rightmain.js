849ac85ede9587ce907bfb0c82e69dd7493b574d
EmrhnOZCN <emrhnozcnceng@gmail.com>
2023-12-01 17:59:59 +0300

    en popüler gündemler eklendi

--- reactsourize/src/components/Rightmain/rightmain.js
+++ reactsourize/src/components/Rightmain/rightmain.js
@@ -3,36 +3,33 @@ import axios from 'axios';
 import 'tailwindcss/tailwind.css';
 
 function Rightmain({ userId }) {
-  const [popularSubreddits, setPopularSubreddits] = useState([]);
+  const [topics, setTopics] = useState([]);
 
-  const fetchPopulars = async () => {
-    try {
-      const response = await axios.get(`http://127.0.0.1:3001/subreddits`);
-      const sortedSubreddits = response.data.sort((a, b) => b.count - a.count);
-      setPopularSubreddits(sortedSubreddits);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get('http://localhost:8080/api/getBestTopics');
+          setTopics(response.data);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
 
-  useEffect(() => {
-    fetchPopulars();
-  }, []);
+      fetchData();
+    }, []);
       return (
         <div className='bg-[#f8f8f8] rounded-md shadow-gray-500 shadow-sm'>
-          <h2 className='text-center bg-orange text-gray-50 text-md font-medium rounded-t-md'>Popüler Subredditler</h2>
+          <h2 className='text-center bg-orange text-gray-50 text-md font-medium rounded-t-md'>Popüler Gündemler</h2>
           <ul className=''>
-            {popularSubreddits.map((subreddit) => (
-              <div key={subreddit._id}>
-                <li className='text-md text-black font-normal mt-1 ml-2'>
-                  <span className='text-orange font-bold'>r/&nbsp;</span>
-                  <span className='text-black'>{subreddit.subreddit}</span>
-                </li>
-              </div>
-            ))}
+             {topics.map((topic) => (
+                      <li key={topic.id}>
+                        <strong>{topic.title}</strong>
+                        <p>Updated Time: {topic.updatedTime}</p>
+                      </li>
+                    ))}
           </ul>
           <div className=' mt-2 border-t p-2'>
-              <p className='text-2xs text-gray-500'>En çok tercih edilen Subredditler</p>
+              <p className='text-2xs text-gray-500'>En çok tercih edilen Gündemler</p>
           </div>
         </div>
 
